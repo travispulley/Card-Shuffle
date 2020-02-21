e97ee84e7288469080126c1de5b7783ffb97ea3d
Travis Pulley <travis@pulley.org>
2020-02-21 15:06:06 -0800

    Added JavaScript riffle and cut

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+# Card Shuffle Suite
 A collection of card shuffling techniques in a variety of languages, frameworks, and tests
 
+## Currently Implemented
+* Javascript
+  * Random Sort
+  * Riffle
+  * Cut
+
+## TODO
+* Standard 52-card Deck Mapping
+* Performance Tests
+* TypeScript Implementation
+* GoLang Implementation
+* C# Implementation
+* Python Implementation
+* Unit Tests
+* Code Cleanup and Organization
+* Fisher-Yates (aka Knuth) shuffle implementation
+  * See https://github.com/Daplie/knuth-shuffle
+  * See https://bost.ocks.org/mike/shuffle/
--- shuffle.js
+++ shuffle.js
@@ -12,3 +12,38 @@ function order_sort(a, b) {
 }
 
 console.log(deck.sort(order_sort))
+
+// split the deck in half, then interlace the cards
+// returns new deck
+function riffle(deck) {
+  const length = deck.length, half = Math.floor(length/2)
+  const outdeck = []
+
+  console.log(length, half)
+
+  for(let x=0; x<half; ++x) {
+    outdeck.push(deck[x])
+    outdeck.push(deck[x+half])
+  }
+  // handle edge case for decks with off number of cards
+  if(deck.length % 2)
+    outdeck.push(deck[deck.length])
+
+  return outdeck
+}
+console.log(riffle(deck))
+
+// reorder the deck along a split point, random if not supplied
+// IDEA: slice instead of splice to preserve input
+// mutates existing deck
+function cut(deck, position) {
+  if(typeof(position) === "undefined")
+    position = Math.floor(Math.random() * deck.length)
+  
+  let removed = deck.splice(position, deck.length-position)
+  // console.log(position, removed, deck)
+  deck.splice(0, 0, ...removed)
+}
+
+cut(deck)
+console.log(deck)
